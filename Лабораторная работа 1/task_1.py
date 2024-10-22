4cb640b4f3f262962a1b010df3131c9224845169
artemluppov <luppovart@gmail.com>
2024-10-22 19:01:12 +0300

    Я изначально не понял, как делать

--- Лабораторная работа 1/task_1.py
+++ Лабораторная работа 1/task_1.py
@@ -1,8 +1,6 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
-n = 0
-for i in numbers:
-    if i != None:
-        n += i
+
+n = sum(numbers[:numbers.index(None)]) + sum(numbers[numbers.index(None) + 1:])
 numbers[numbers.index(None)] = n/len(numbers)
 # TODO заменить значение пропущенного элемента средним арифметическим
 
